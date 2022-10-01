a4c2760e4fd1ce48173ba48fb0ae7c14f3089284
Matt Trice <matt@trice.design>
2022-10-01 09:36:16 -0400

    broke it

--- barba-script.js
+++ barba-script.js
@@ -6,33 +6,7 @@ $(document).on("click", function (e) {
   percentTop = (mouseTop / $(window).height()) * 100;
   percentLeft = (mouseLeft / $(window).width()) * 100;
 });
-
-function updateCurrentClass() {
-  $(".w--current").removeClass("w--current");
-  $(".nav a").each(function (index) {
-    if ($(this).attr("href") === window.location.pathname) {
-      $(this).addClass("w--current");
-    }
-  });
-}
-
-barba.init({
-  preventRunning: true,
-  transitions: [
-    {
-      enter(data) {
-        updateCurrentClass();
-        gsap.defaults({
-          ease: "power2.inOut",
-          duration: 1.2
-        });
-        if ($(".menu_link.w--current").length > 0) {
-          // If going to internal page
-          gsap.fromTo(".is-home", { x: "0%" }, { x: "49%" });
-          gsap.fromTo(".is-about", { x: "-49%" }, { x: "0%" });
-        } else {
-          // If going to the homepage
-          gsap.fromTo(".is-home", { x: "49%" }, { x: "0%" });
+"49%" }, { x: "0%" });
           gsap.fromTo(".is-about", { x: "0%" }, { x: "-49%" });
         }
 
