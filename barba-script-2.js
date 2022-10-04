94e326fcaa219fa62e3c22739f9d8ebd7a4d99c9
Matt Trice <matt@trice.design>
2022-10-04 10:47:10 -0400

    change back to y

--- barba-script-2.js
+++ barba-script-2.js
@@ -40,7 +40,7 @@ barba.init({
         return tl.from(data.next.container, {
           zIndex: 3,
           borderRadius: "5%",
-          zPercent: -100,
+          yPercent: 100,
           duration: 0.5,
           ease: "power2.out"
         });
